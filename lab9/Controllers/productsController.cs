2468f5b489230042a2c8230bc6afa44a231ff5ba
DavidQuispeSolier <david.quispe.s@tecsup.edu.pe>
2023-05-22 21:57:21 -0500

    Agregar archivos de proyecto.

---
+++ lab9/Controllers/productsController.cs
@@ -0,0 +1,19 @@
+﻿using lab9.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab9.Controllers
+{
+    public class productsController : Controller
+    {
+        private NeptunoEntities db = new NeptunoEntities();
+        // GET: productos
+        public ActionResult Index()
+        {
+            return View(db.productos.ToList());
+        }
+    }
+}
